556070c041f6a881f0d3f394e2d78a0a5ea96138
Felipe Lugão Eccard <feccard@muxi.com.br>
2018-03-16 15:56:50 -0300

    Adds mainActivity.

---
+++ app/src/main/java/muxi/pwpservice/MainActivity.java
@@ -0,0 +1,26 @@
+package muxi.pwpservice;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @OnClick(R.id.button)
+    public void onClickGoToPayment(View v) {
+        Intent intent = new Intent(this,PaymentActivity.class);
+        startActivity(intent);
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+}
